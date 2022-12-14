97e922d892ee8adf3dfd87a6d1cebf76d7c0f945
KleilaGj <kleila.gjeta@gmail.com>
2022-12-14 10:23:23 +0000

    challenges 1-3

--- functions.js
+++ functions.js
@@ -1,3 +1,18 @@
+//Activity 1
+const sayHello = () => {
+	console.log("Hi there, how are you?")
+}
+sayHello()
+
+
+//Activity 2
+const sayHello = (name, drink) => {
+	console.log(`Hi there ${name}, would you like to drink some ${drink}?`)
+}
+sayHello("Bob", "Cocacola")
+
+
+//Activity 3
 function multiply(num1, num2) {
     return num1 * num2
 }
